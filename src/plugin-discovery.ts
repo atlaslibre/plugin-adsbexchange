7166718bd1d7a0956e74254812f660b38f4271ac
Christian Panton <christian@panton.org>
2025-04-14 12:38:19 +0200

    add attribution

--- src/plugin-discovery.ts
+++ src/plugin-discovery.ts
@@ -3,6 +3,7 @@ import { registerPlugin } from "./shared/registration";
 registerPlugin({
   type: "actor",
   name: "ADSB Exchange",
+  attribution: "Aircraft data by ADSBExchange.com",
   replay: true,
   locate: true,
   status: true
